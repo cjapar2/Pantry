0e714e1c134f2378323bfb048a00e35363e908da
CJ Apar <capar@ucsc.edu>
2022-11-29 13:34:30 -0800

    Added frontend test files.

--- frontend/src/components/__tests__/App.test.js
+++ frontend/src/components/__tests__/App.test.js
@@ -1,10 +1,25 @@
-import {render} from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom';
+import {screen, waitFor} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import App from '../App';
 
 /**
  */
  test('App Renders', async () => {
+  console.log('test');
   render(<App />);
+});
+
+test('Sucess', async () => {
+  render(<App />);
+  const email = screen.getByPlaceholderText("Email");
+  await userEvent.type(email, 'jsmith0@ucsc.edu');
+  const passwd = screen.getByPlaceholderText('Password');
+  await userEvent.type(passwd, '1234');
+  fireEvent.click(screen.getByText('Login'));
+  await waitFor(() => {
+    expect(localStorage.getItem('user')).not.toBe(null);
+  });
 });
