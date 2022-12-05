8ff7fe6579de5c3e5e224fb8a7fe80eb269a1a4b
CJ Apar <capar@ucsc.edu>
2022-12-05 14:07:13 -0800

    Added more backend tests

--- backend/src/__tests__/backend.test.js
+++ backend/src/__tests__/backend.test.js
@@ -32,6 +32,13 @@ beforeEach(async () => {
     });
 });
 
+// beforeEach( async () => {
+//   await request.post('/v0/food')
+//     .auth(token, {type: 'bearer'})
+//     .send(testDeletedFood)
+//     .expect(201);
+// });
+
 test('GET Invalid URL', async () => {
   await request.get('/v0/so-not-a-real-end-point-ba-bip-de-doo-da/')
     .expect(404);
@@ -77,7 +84,8 @@ const testFood =  {
   'item': 'cookies',
   'amount': 100,
   'purchaseDate': '2022-12-24',
-  'notes': 'cookies for santa'
+  'notes': 'cookies for santa',
+  'tags': {}
 };
 
 test('POST /food', async () => {
@@ -104,6 +112,7 @@ test('POST /signup (Create new account)', async () => {
   .expect(201)
 });
 
+
 test('GET /signup', async () => {
   await request.get('/v0/signup')
   .expect(200)
@@ -112,4 +121,91 @@ test('GET /signup', async () => {
     expect(res.body).toBeDefined();
     expect(res.body[1].usr.name).toEqual('CJ');
   });
+});
+
+const newList = {
+  "list_name": "new_list"
+}
+
+const userId = 1;
+test('POST /lists (Post a new list)', async () => {
+  await request.post('/v0/lists/' + userId)
+  .send(newList)
+  .expect(201)
+});
+
+test('GET /lists', async () => {
+  await request.get('/v0/lists/' + userId)
+  .auth(token, {type: 'bearer'})
+  .expect(200)
+  .then((res) => {
+    expect(res).toBeDefined();
+    expect(res.body).toBeDefined();
+    expect(res.body[0].list_name).toEqual('new_list')
+  });
+});
+
+const userList = {
+  "usr_id": 1,
+  "list_id": 1
+}
+
+test('POST /users_lists', async() => {
+  await request.post('/v0/users_lists/')
+    .send(userList)
+    .expect(201)
+});
+
+test('GET /users_lists', async() => {
+  await request.get('/v0/users_lists/')
+    .expect(200)
+    .then((res) => {
+      expect(res).toBeDefined();
+      expect(res.body).toBeDefined();
+      expect(res.body[1].id).toEqual(2);
+    });
+});
+
+
+test('POST /foodInList (Add food item to new list)', async() => {
+  await request.post('/v0/foodInList/' + listId)
+    .auth(token, {type: 'bearer'})
+    .send(testFood)
+    .expect(201)
+});
+
+test('GET /foodInList', async() => {
+  await request.get('/v0/foodInList/' + listId)
+    .auth(token, {type: 'bearer'})
+    .expect(200)
+});
+
+const testFood2 =  {
+  'item': 'eggnog',
+  'amount': 1,
+  'purchaseDate': '2000-12-24',
+  'notes': 'eggnog'
+};
+
+const testFood2_put =  {
+  'item': 'eggnog',
+  'amount': 1,
+  'purchaseDate': '2000-12-24',
+  'notes': 'eggnog spoiled'
+};
+
+test('POST /food', async () => {
+  await request.post('/v0/food/')
+    .auth(token, {type: 'bearer'})
+    .send(testFood2)
+    .expect(201);
+});
+
+const listId = 1;
+
+test('DELETE /food (Delete food item)', async () => {
+  await request.delete('/v0/food/' + listId)
+    .auth(token, {type: 'bearer'})
+    .send(testFood2_put)
+    .expect(204);
 });
